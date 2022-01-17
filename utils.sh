c346fd69bda8259ebcf6966aaa9c89b1e9fa44cf
burakurer <58211081+burakurer@users.noreply.github.com>
2022-01-18 01:24:36 +0300

    v1.1.1
    aaPanel kurulumu eklendi

--- utils.sh
+++ utils.sh
@@ -3,8 +3,8 @@
 #######################################################
 #                    burakurer.com                    #
 #                                                     #
-#     Version     : 1.1                               #
-#     Last Update : 15/01/2022                        #
+#     Version     : 1.1.1                             #
+#     Last Update : 18/01/2022                        #
 #     Website     : https://burakurer.com             #
 #     Github      : https://github.com/burakurer      #
 #                                                     #
@@ -28,15 +28,23 @@ pleskInstall() {
     sh <(curl https://autoinstall.plesk.com/one-click-installer || wget -O – https://autoinstall.plesk.com/one-click-installer)
 }
 
+aaPanelInstall() {
+    printf "\E[31maaPanel kurulumu baslatiliyor\E[0m\n(Iptal icin ctrl+c)\n"
+    sleep 5
+    yum install -y wget && wget -O install.sh http://www.aapanel.com/script/install_6.0_en.sh && bash install.sh
+}
+
 while :
 do
     printf "\E[31mBu bash scripti burakurer.com tarafindan yazilmistir\E[0m\n"
-    echo -e "\n[1] Sistem guncellemelerini denetle\n[2] Plesk kur"
+    echo -e "\n[1] Sistem guncellemelerini denetle\n[2] Plesk kur\n[3] aaPanel kur"
     read r
     if [ $r == 1 ]; then
         updateSystem
     elif [ $r == 2 ]; then
         pleskInstall
+    elif [ $r == 3 ]; then
+        aaPanelInstall
     else
         echo "Gecerli bir secenek girilmedi!"
         exit
